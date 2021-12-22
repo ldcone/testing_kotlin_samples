ae835e96bc5b2d27eb5e3873bd76ba83b6205a92
sophiainailys <sheisinthezone@gmail.co>
2021-12-22 15:16:33 +0900

    branchmade_openapi

---
+++ app/src/main/java/com/example/testing_kotlin_samples/bookreview/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.testing_kotlin_samples.bookreview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
