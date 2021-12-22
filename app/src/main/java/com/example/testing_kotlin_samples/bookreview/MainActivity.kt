dd858c6b2c1491f08b230856f18d7e63124b73c8
ldcone <dongcheonlee@naver.com>
2021-12-22 15:32:24 +0900

    branchmade_openapi

--- app/src/main/java/com/example/testing_kotlin_samples/bookreview/MainActivity.kt
+++ app/src/main/java/com/example/testing_kotlin_samples/bookreview/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.testing_kotlin_samples.bookreview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.testing_kotlin_samples.R
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
