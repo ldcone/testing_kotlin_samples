c8d15972fb0f74e6376f82761ed1452c1a68915d
ldcone <62379711+ldcone@users.noreply.github.com>
2021-12-25 00:00:00 +0900

    cloneapp_tinder_companion object make

---
+++ app/src/main/java/com/example/testing_kotlin_samples/tinder/DBKey.kt
@@ -0,0 +1,10 @@
+package com.example.testing_kotlin_samples.tinder
+
+class DBKey {
+    companion object{
+        const val USERS = "Users"
+        const val LIKED_BY = "likedBy"
+        const val LIKE = "like"
+        const val DIS_LIKE ="disLike"
+    }
+}
