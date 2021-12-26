a1af776e54792c13d85fd07a92824da97f968fa9
ldcone <62379711+ldcone@users.noreply.github.com>
2021-12-27 02:06:27 +0900

    implement tradeApp_fragment_home_firebase_connected

---
+++ app/src/main/java/com/example/testing_kotlin_samples/tradeapp/DBKey.kt
@@ -0,0 +1,7 @@
+package com.example.testing_kotlin_samples.tradeapp
+
+class DBKey {
+    companion object{
+        const val DB_ARTICLES="Articles"
+    }
+}
--- app/src/main/java/com/example/testing_kotlin_samples/tradeapp/home/HomeFragment.kt
+++ app/src/main/java/com/example/testing_kotlin_samples/tradeapp/home/HomeFragment.kt
@@ -6,23 +6,81 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.testing_kotlin_samples.R
 import com.example.testing_kotlin_samples.databinding.FragmentHomeBinding
+import com.example.testing_kotlin_samples.tradeapp.DBKey.Companion.DB_ARTICLES
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment:Fragment(R.layout.fragment_home) {
+    private lateinit var articleDB: DatabaseReference
     private lateinit var binding:FragmentHomeBinding
     private lateinit var articleAdapter:ArticleAdapter
+
+    private val articlList= mutableListOf<ArticleModel>()
+
+    private val listener = object :ChildEventListener{
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            articleModel ?: return
+
+            articlList.add(articleModel)
+            articleAdapter.submitList(articlList)
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+
+        }
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {
+
+        }
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+
+        }
+
+    }
+
+    private val auth: FirebaseAuth by lazy{
+        Firebase.auth
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
 
+        articleDB = Firebase.database.reference.child(DB_ARTICLES)
+        //view 재사용 과정에서 생기는 중복 피함
+        articlList.clear()
+
         articleAdapter = ArticleAdapter()
-        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
-            add(ArticleModel("0","지정원",100000,"100000000000000원",""))
-            add(ArticleModel("1","이동천",200000,"100000000000000원",""))
-        })
+
         binding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
         binding.articleRecyclerView.adapter = articleAdapter
 
+        articleDB.addChildEventListener(listener)
+
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        articleAdapter.notifyDataSetChanged()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        articleDB.removeEventListener(listener)
+    }
+
 }
